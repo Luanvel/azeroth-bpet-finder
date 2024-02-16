876ac7548c1b82fd69ebf183783fcf4197d0cdc0
Veronica Ruiz <veronica.ruizab@gmail.com>
2024-02-16 11:08:10 +0100

    fix bug favoritepets

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -32,7 +32,7 @@ async function setupPage() {
   let favoritePets = localStorage.getItem("favoritePets");
   favoritePets = JSON.parse(favoritePets);
 
-  if (favoritePets.length == 0 || !favoritePets) {
+  if (!favoritePets || favoritePets.length == 0) {
     document.querySelector("#favorite_warning").style.display = "inline-block";
   } else {
     // Iterar sobre cada petId y cargar la mascota favorita
